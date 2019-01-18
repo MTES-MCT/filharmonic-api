b78799032a8191f9f22c8aa274dbc447537eeae5
Maxime Dréau <maxime@dreau.fr>
2019-01-18 18:05:09 +0100

    Ajout du service emails (non intégré)

---
+++ emails/emails_test.go
@@ -0,0 +1,41 @@
+package emails
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestSend(t *testing.T) {
+	assert := require.New(t)
+
+	// start a fake MailJet API
+	go func() {
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte(`{"success": true}`))
+		})
+		http.ListenAndServe(":5100", nil)
+	}()
+
+	emailConfig := Config{
+		FromEmail:     "contact@filharmonic.beta.gouv.fr",
+		FromName:      "Fil'Harmonic",
+		APIPublicKey:  "publickey",
+		APIPrivateKey: "privatekey",
+	}
+	emailService := New(emailConfig)
+	emailService.SetBaseURL("http://localhost:5100/")
+
+	err := emailService.Send(Email{
+		Subject:        "Test Email",
+		RecipientEmail: "sample-user@filharmonic.beta.gouv.fr",
+		RecipientName:  "Sample user",
+		Template:       NotificationSummary,
+		Variables: map[string]interface{}{
+			"name":            "Michel",
+			"nbNotifications": 3,
+		},
+	})
+	assert.NoError(err)
+}
---
+++ emails/templates.go
@@ -0,0 +1,8 @@
+package emails
+
+var (
+	NotificationSummary = emailTemplate{
+		Name:      "NotificationSummary",
+		MailJetID: 656422, // defined in MailJet
+	}
+)
