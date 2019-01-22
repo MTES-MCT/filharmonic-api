5438e9d2c833be1e51ead19317fba460ae171ca3
Maxime Dréau <maxime@dreau.fr>
2019-01-22 15:40:07 +0100

    Fix CI script dump_pg_schema

--- database/scripts/dump_pg_schema.sh
+++ database/scripts/dump_pg_schema.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-docker-compose exec db su - postgres -c "pg_dump --schema-only --create --no-owner --clean --exclude-table=gopg_migrations filharmonic" | sed '/^--/d' | perl -0pe 's/CREATE SEQUENCE public\.gopg_migrations_id[^;]*;//' | sed '/^\s*$/d' | grep -v gopg_migrations
+PGPASSWORD=filharmonic pg_dump -h localhost -p 5432 -U filharmonic --schema-only --create --no-owner --clean --exclude-table=gopg_migrations filharmonic | sed '/^--/d' | perl -0pe 's/CREATE SEQUENCE public\.gopg_migrations_id[^;]*;//' | sed '/^\s*$/d' | grep -v gopg_migrations
