07777cc7822b2a489f09168d5b7eb7851b3164c0
Tristan Robert <tristan.robert.44@gmail.com>
2019-03-29 09:30:12 +0100

    Ajout de la requete sql des statistiques

---
+++ database/stats/stats.sql
@@ -0,0 +1,4 @@
+COPY ( select inspection.etablissement_id, point_de_controle.id as point_de_controle_id, constat.type as constat_type, constat.date_resolution
+from inspections as inspection
+join point_de_controles as point_de_controle on point_de_controle.inspection_id = inspection.id
+left join constats as constat on point_de_controle.constat_id = constat.id ) TO STDOUT WITH CSV HEADER;
