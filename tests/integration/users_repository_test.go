46bb593927578c14b308ce6a835510ea8f55ae84
Tristan Robert <tristan.robert.44@gmail.com>
2019-04-09 15:36:54 +0200

    Refactor users repository test: données propres aux tests

--- tests/integration/users_repository_test.go
+++ tests/integration/users_repository_test.go
@@ -3,27 +3,52 @@ package integration
 import (
 	"testing"
 
+	"github.com/MTES-MCT/filharmonic-api/models"
 	"github.com/MTES-MCT/filharmonic-api/tests"
 )
 
 func TestCheckUsersInProfilInspecteur(t *testing.T) {
-	assert, application := tests.InitDB(t)
+	assert, application := tests.InitEmptyDB(t)
 
-	checked, err := application.Repo.CheckUsersInspecteurs([]int64{3, 4})
+	users := []models.User{
+		models.User{
+			Email:   "inspecteur1@filharmonic.com",
+			Profile: models.ProfilInspecteur,
+		},
+		models.User{
+			Email:   "inspecteur2@filharmonic.com",
+			Profile: models.ProfilInspecteur,
+		},
+	}
+	assert.NoError(application.DB.Insert(&users))
+
+	checked, err := application.Repo.CheckUsersInspecteurs([]int64{1, 2})
 	assert.True(checked)
 	assert.NoError(err)
 }
 
 func TestCheckUsersWithProfilExploitant(t *testing.T) {
-	assert, application := tests.InitDB(t)
+	assert, application := tests.InitEmptyDB(t)
 
-	checked, err := application.Repo.CheckUsersInspecteurs([]int64{1, 4})
+	users := []models.User{
+		models.User{
+			Email:   "inspecteur1@filharmonic.com",
+			Profile: models.ProfilInspecteur,
+		},
+		models.User{
+			Email:   "exploitant1@filharmonic.com",
+			Profile: models.ProfilExploitant,
+		},
+	}
+	assert.NoError(application.DB.Insert(&users))
+
+	checked, err := application.Repo.CheckUsersInspecteurs([]int64{1, 2})
 	assert.False(checked)
 	assert.NoError(err)
 }
 
 func TestCheckUsersNotExisting(t *testing.T) {
-	assert, application := tests.InitDB(t)
+	assert, application := tests.InitEmptyDB(t)
 
 	checked, err := application.Repo.CheckUsersInspecteurs([]int64{4000})
 	assert.False(checked)
